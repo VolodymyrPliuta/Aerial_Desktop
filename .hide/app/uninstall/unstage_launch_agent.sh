9822ba6b2c92b42d26453b5214e7b60d70f24cea
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-26 13:55:12 -0400

    battery watch launch agent, install/script/uninstall/plist;

--- .hide/app/uninstall/unstage_launch_agent.sh
+++ .hide/app/uninstall/unstage_launch_agent.sh
@@ -2,4 +2,6 @@
 
  launchctl unload ~/Library/LaunchAgents/com.example.hello.plist;
  rm ~/Library/LaunchAgents/com.example.hello.plist
+ launchctl unload ~/Library/LaunchAgents/com.example.charg.plist;
+ rm ~/Library/LaunchAgents/com.example.charg.plist
  # mv ~/Library/LaunchAgents/com.example.hello.plist com.example.hello.plist;
